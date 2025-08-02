3c4dd8f93988ced682ab61aeae064e325071ec15
Alekhya <alekhyanimmala4001@gmail.com>
2025-08-02 15:50:39 +0530

    Update miniProject.java

--- miniProject.java
+++ miniProject.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 
 public class miniProject {
     public static void main(String[] args) {
-        // Mini project
+        // My First project
+        // Guess the number
         Scanner sc = new Scanner(System.in);
-        int myNumber = (int)(Math.random()*1000);
+        int myNumber = (int)(Math.random()*1000); // The random number is from 1- 1000
         int userNumber = 0;
 
         do {
@@ -23,9 +24,10 @@ public class miniProject {
             else {
                 System.out.println("Your number is too small");
             }
-        } while(userNumber >= 0);
+        } while(userNumber >= 0);  // If you give -ve number, It means u want to  know the number
         System.out.println("My number was: ");
         System.out.println(myNumber);
 
     }
 }
+
